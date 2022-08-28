25a0ab6f344cfac7f31d3191b65183a6e66a74ca
Ibrahima Assiatou Diallo[OGN DSI/DDF/SAI] <DIALLO110632@orange-sonatel.com>
2022-08-28 19:55:35 +0000

    first push from origin

---
+++ src/server.ts
@@ -0,0 +1,71 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+
+  // the endpoint for the todo
+  app.get("/filteredimage", async (req, res) => {
+    // destruct our path params
+    let image_url = req.query.image_url;
+
+    if (!image_url) {
+      return res.status(400).send('no parameter defined');
+    } else {
+      //validate the image_url query
+      const isUrlValide = image_url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+      if (isUrlValide == null)
+        return res.status(400).send(`Inavlid url! Try again with valid url`);
+       else{
+          //call filterImageFromURL(image_url) to filter the image
+          const filteredImage : string = await filterImageFromURL(image_url);
+        if(filteredImage===undefined || filteredImage===null || filteredImage === '')
+          return res.status(400).send(`No image found try again with another url`);
+        else{
+          //deletes any files on the server on finish of the response
+          res.on('finish', function() {
+            deleteLocalFiles([filteredImage]);  
+          });
+          //send the resulting file in the response
+          return res.status(200).sendFile(filteredImage);
+
+        }
+
+      }
+    }
+
+  });
+
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
